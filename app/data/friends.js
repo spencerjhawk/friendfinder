214be8e248788d568db03f739636428271cc1719
Spencer Hawk <spencerjhawk@gmail.com>
2017-12-04 15:08:55 -0600

    added folders and friends file

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Carter',
+		photo: 'https://static.hudl.com/users/prod/10369134_2f7cf676ee5f4a869e359beaa3cf2a42.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Michelle',
+		photo: 'https://www.famousbirthdays.com/headshots/michelle-i-williams-1.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Martin',
+		photo: 'http://static.tvtropes.org/pmwiki/pub/images/MartinLawrence_2476.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Mary',
+		photo: 'https://www.marytarotreadings.com/wp-content/uploads/2016/09/MaryLougland450.png',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Curt',
+		photo: 'https://www.gannett-cdn.com/-mm-/cace58a202da49400927f49b69379013a932916f/c=0-12-625-481&r=x404&c=534x401/local/-/media/2015/05/20/IowaCity/B9317421681Z.1_20150520175803_000_G78ARHUFD.1-0.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Scott',
+		photo: 'https://www.brookings.edu/wp-content/uploads/2016/06/scott26-e1471446001930.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Mr. Mean',
+		photo: 'https://media1.fdncms.com/metrotimes/imager/u/original/2484735/shutterstock_569555101.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Big Time Bobby',
+		photo: 'https://i.ytimg.com/vi/sOMkcJy-i4Q/hqdefault.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jared',
+		photo: 'http://thefederalist.com/wp-content/uploads/2017/01/headshot.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Terrible Tommy',
+		photo: 'http://www.managingcommunities.com/wp-content/uploads/2013/12/mean-guy.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
