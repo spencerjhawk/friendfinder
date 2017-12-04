0b2d6394656dcf2427d1c6c67dc3d5c8cdd88bdf
Spencer Hawk <spencerjhawk@gmail.com>
2017-12-04 16:11:23 -0600

    fixed route typo

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
 
 	// Home page
 	app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
+		res.sendFile(path.join(__dirname, '../public/index.html'));
 	});
 
 	// Survey page
